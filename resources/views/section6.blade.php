9bbdf7b59f04c71eb2b758064d556937189363f9
Andika Pranayoga <praandikayoga@gmail.com>
2021-02-25 14:43:38 +0800

    Menambahkan section 6 dan membuat js hitung simulasi kredit

---
+++ resources/views/section6.blade.php
@@ -0,0 +1,59 @@
+<section class="section6">
+    <div class="container">
+        <div class="row justify-content-md-center title">
+            <h3>KONTAK</h3>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <img src="{{ asset('image/ic-maps.png') }}" alt="" class="img-fluid alamat">
+                <p class="sub-title">
+                    Alamat
+                </p>
+                <p>
+                    Jl. Teuku Umar Barat
+                    100X Malboro
+                    Denpasar
+                </p>
+                <br><br>
+                <img src="{{ asset('image/ic-float-wa-1.png') }}" alt="" class="img-fluid wa">
+                <p class="sub-title">
+                    Whatsapp
+                </p>
+                <p>
+                    0812-4657-1111
+                </p>
+            </div>
+            <div class="col-md-8">
+                <div class="mapouter">
+                    <div class="gmap_canvas">
+                        <iframe width="100%" height="500px" id="gmap_canvas"
+                            src="https://maps.google.com/maps?q=Jl.%20Teuku%20Umar%20100X%20Malboro&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                            frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                        </iframe>
+                        <a
+                            href="https://yt2.org/de/youtube-to-mp3-ALeKk00qEW0sxByTDSpzaRvl8WxdMAeMytQ1611842368056QMMlSYKLwAsWUsAfLipqwCA2ahUKEwiikKDe5L7uAhVFCuwKHUuFBoYQ8tMDegUAQCSAQCYAQCqAQdnd3Mtd2l6">
+                        </a>
+                        <br>
+                        <style>
+                            .mapouter {
+                                position: relative;
+                                text-align: right;
+                                height: 500px;
+                                width: 100%;
+                            }
+                        </style>
+                        <style>
+                            .gmap_canvas {
+                                overflow: hidden;
+                                background: none !important;
+                                height: 500px;
+                                width: 100%;
+                            }
+                        </style>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
